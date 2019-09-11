c787c9dc5fe8823244411d8dfa7ca50c7d898a53
Jonathan Lovera <jon@jonlovera.com>
2019-09-12 00:48:47 +1000

    Updates

---
+++ precache-manifest.4246cb7fe0b2058c11fb5a5e40050253.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe07483f15810ec3d363895feb16c96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6391bf95ce14a0f47774",
+    "url": "/static/css/main.72a11327.chunk.css"
+  },
+  {
+    "revision": "ff54038c737eb087385d",
+    "url": "/static/js/2.a7843669.chunk.js"
+  },
+  {
+    "revision": "6391bf95ce14a0f47774",
+    "url": "/static/js/main.07ee24c4.chunk.js"
+  },
+  {
+    "revision": "07367699fa449e5a0223",
+    "url": "/static/js/runtime~main.b5119747.js"
+  },
+  {
+    "revision": "1a201b8200fc59af5a3182f5653e56c2",
+    "url": "/static/media/angelina.1a201b82.png"
+  },
+  {
+    "revision": "cae4ca7408ae7833e796f5bc57d7db2f",
+    "url": "/static/media/coding.cae4ca74.png"
+  },
+  {
+    "revision": "2396e739ef918acd071167ef4ceb615b",
+    "url": "/static/media/design.2396e739.png"
+  },
+  {
+    "revision": "83f8a60f9cc99b55127ddf4e86e2f3a7",
+    "url": "/static/media/divorce-negotiations.83f8a60f.png"
+  },
+  {
+    "revision": "14c590d124662eb50efa4c00f027b79c",
+    "url": "/static/media/fa-brands-400.14c590d1.eot"
+  },
+  {
+    "revision": "3e1b2a654a784ceb385157140b4ccd71",
+    "url": "/static/media/fa-brands-400.3e1b2a65.woff2"
+  },
+  {
+    "revision": "5e8aa9ea0ebcd2218178f554cdd8e6b0",
+    "url": "/static/media/fa-brands-400.5e8aa9ea.ttf"
+  },
+  {
+    "revision": "bfa9c38bd6081dafe7278dedc4aad1d9",
+    "url": "/static/media/fa-brands-400.bfa9c38b.svg"
+  },
+  {
+    "revision": "df02c782834b113d605d8329368737b4",
+    "url": "/static/media/fa-brands-400.df02c782.woff"
+  },
+  {
+    "revision": "3ded831d708bf90b2da86756551b5c1c",
+    "url": "/static/media/fa-solid-900.3ded831d.woff"
+  },
+  {
+    "revision": "42e1fbd2cf655a0b44a2dfae9ca2f8c1",
+    "url": "/static/media/fa-solid-900.42e1fbd2.eot"
+  },
+  {
+    "revision": "6ed5e3bc9018d2a03e2f1fadbf8a0a4a",
+    "url": "/static/media/fa-solid-900.6ed5e3bc.svg"
+  },
+  {
+    "revision": "896e20e26ad07dc63c9526ed814117e9",
+    "url": "/static/media/fa-solid-900.896e20e2.ttf"
+  },
+  {
+    "revision": "d6d8d5da9214dc7d46b297672a602d55",
+    "url": "/static/media/fa-solid-900.d6d8d5da.woff2"
+  },
+  {
+    "revision": "9abce907061e2b6471d83559d0d9593a",
+    "url": "/static/media/flowerup.9abce907.png"
+  },
+  {
+    "revision": "a0b0e884f0a3fff05932299b4c88eb06",
+    "url": "/static/media/jhon-music.a0b0e884.png"
+  },
+  {
+    "revision": "d5628f3a644a4938b1e18346d6f631c7",
+    "url": "/static/media/klobic.d5628f3a.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "c495513b6fe0303f50cd5570c397f06e",
+    "url": "/static/media/rudimentary.c495513b.png"
+  },
+  {
+    "revision": "bca0ed6f21737162adc522e0f1d9bfe8",
+    "url": "/static/media/yesy-milano.bca0ed6f.png"
+  }
+]);
